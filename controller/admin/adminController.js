4457bcdae64951edadaa5bb0d6c9224ae6f0513f
Ala Raihan <alaraihan77@gmail.com>
2026-02-25 14:05:57 +0530

    completed all my pendings

--- controller/admin/adminController.js
+++ controller/admin/adminController.js
@@ -349,6 +349,7 @@ export const getDashboardData = async (req, res) => {
       statusDistribution,
       salesChartData,
       topProducts,
+      paymentDistribution,
     });
 
   } catch (error) {
